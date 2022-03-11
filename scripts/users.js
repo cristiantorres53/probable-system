f7610903de6c1cae6e4102ab88466f30da98303b
Cristian Camilo Torres Rodriguez <47953810+cristiantorres53@users.noreply.github.com>
2022-03-11 17:41:35 -0500

    agregan parametros al metodos POST

--- scripts/users.js
+++ scripts/users.js
@@ -5,12 +5,18 @@ const btnCrearUsuario = document.getElementById('btncrear')
 formulario.addEventListener('submit', async (e) => {
     e.preventDefault()
 
+    let foto = document.getElementById('foto').value
     let name = document.getElementById('name').value
+    let email = document.getElementById('email').value
+    let description = document.getElementById('description').value
 
     let resp = await fetch (url, {
         method: 'POST',
         body: JSON.stringify({
-            
+            foto: url,
+            name : name,
+            email: email,
+            description:description
         }),
         headers: {
             "Content-Type" : "aplication/json; charset=UTF-8"
